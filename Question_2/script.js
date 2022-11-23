6680d7e2c37e28c6259ae379cafdaf3466926e01
Adesh Nayak <adeshnayak6@gmail.com>
2022-11-24 00:15:28 +0530

    saved

--- Question_2/script.js
+++ Question_2/script.js
@@ -1,7 +1,3 @@
-let searchInput = document.querySelector("input");
-let inp = searchInput.value;
-searchInput.addEventListener('change', ()=> console.log(inp))
-
 fetch(
     "https://raw.githubusercontent.com/debojyoti/places-fake-rest-api/master/cafes.json"
 )
@@ -11,24 +7,25 @@ fetch(
     .then((data) => {
         let cafeNames = data.cafes;
 
-        fetch('https://raw.githubusercontent.com/debojyoti/places-fake-rest-api/master/places.json').then(response => response.json()).then(placeData => {
-            let cafePlaces = placeData.places;
+        fetch(
+            "https://raw.githubusercontent.com/debojyoti/places-fake-rest-api/master/places.json"
+        )
+            .then((response) => response.json())
+            .then((placeData) => {
+                let cafePlaces = placeData.places;
 
-            let mergedData = cafeNames.map(item => {
-                let otherData = cafePlaces.find(element => element.id === item.location_id)
-                return {...item, ...otherData}
-            })
+                let mergedData = cafeNames.map((item) => {
+                    let otherData = cafePlaces.find(
+                        (element) => element.id === item.location_id
+                    );
+                    return { ...item, ...otherData };
+                });
 
-
-            // let filteredCafes = mergedData.filter(item => item['name'].includes(searchInput.value))
-            // console.log(filteredCafes);
-
-            let tableBody = document.querySelector("tbody");
-            let tableData = "";
-            let i = 1;
-            mergedData.map(value => {
-
-                tableData += `<tr>
+                let tableBody = document.querySelector("tbody");
+                let tableData = "";
+                let i = 1;
+                mergedData.map((value) => {
+                    tableData += `<tr>
                 <td class="column1"> ${i++} </td>
                 <td class="column2">${value.name}</td>
                 <td class="column3">
@@ -37,10 +34,8 @@ fetch(
                 <td class="column4">${value.postal_code}</td>
                 <td class="column5">${value.lat} </td>
                 <td class="column6"> ${value.long}</td>
-            </tr>`
-            })
-            tableBody.innerHTML = tableData;
-
-            
-        })
+            </tr>`;
+                });
+                tableBody.innerHTML = tableData;
+            });
     });
