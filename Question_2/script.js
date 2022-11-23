694dac5ef475632c0882f6c36b329e32b9930c3a
Adesh Nayak <adeshnayak6@gmail.com>
2022-11-24 00:03:38 +0530

    cafes added

---
+++ Question_2/script.js
@@ -0,0 +1,46 @@
+let searchInput = document.querySelector("input");
+let inp = searchInput.value;
+searchInput.addEventListener('change', ()=> console.log(inp))
+
+fetch(
+    "https://raw.githubusercontent.com/debojyoti/places-fake-rest-api/master/cafes.json"
+)
+    .then((res) => {
+        return res.json();
+    })
+    .then((data) => {
+        let cafeNames = data.cafes;
+
+        fetch('https://raw.githubusercontent.com/debojyoti/places-fake-rest-api/master/places.json').then(response => response.json()).then(placeData => {
+            let cafePlaces = placeData.places;
+
+            let mergedData = cafeNames.map(item => {
+                let otherData = cafePlaces.find(element => element.id === item.location_id)
+                return {...item, ...otherData}
+            })
+
+
+            // let filteredCafes = mergedData.filter(item => item['name'].includes(searchInput.value))
+            // console.log(filteredCafes);
+
+            let tableBody = document.querySelector("tbody");
+            let tableData = "";
+            let i = 1;
+            mergedData.map(value => {
+
+                tableData += `<tr>
+                <td class="column1"> ${i++} </td>
+                <td class="column2">${value.name}</td>
+                <td class="column3">
+                    ${value.street_no} ${value.locality}
+                </td>
+                <td class="column4">${value.postal_code}</td>
+                <td class="column5">${value.lat} </td>
+                <td class="column6"> ${value.long}</td>
+            </tr>`
+            })
+            tableBody.innerHTML = tableData;
+
+            
+        })
+    });
